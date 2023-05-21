f6d024704978b5c30b72b8360e2df06abf16454a
kevin-krause <contatokevinkrause@gmail.com>
2023-05-21 02:59:47 -0300

    header & ordem de serviço

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,22 +4,28 @@ import { DiJsBadge } from 'react-icons/di'
 
 export const Header = () => {
     return (
-        <div className="max-w-full p-6 flex items-center ml-6 mr-6 mt-3 bg-gradient-to-tl from-gray-900 via-gray-900 to-black rounded-lg text-zinc-100">
+        <div className="max-w-full p-6 flex items-center ml-6 mr-6 mt-3 bg-gradient-to-tl from-gray-900 via-gray-900 to-black rounded-lg text-zinc-100 justify-between">
             <a className="mr-6" href={'/'}>
-                <DiJsBadge className="w-[30px] h-[30px] text-sky-300" />
+                <img
+                    className="w-[100px] h-[16x]"
+                    src=".\favicon.ico"
+                    alt="logo"
+                />
             </a>
 
-            <a className="mr-6" href={'/'}>
-                Dashboard
-            </a>
+            <div>
+                <a className="mr-6" href={'/'}>
+                    Dashboard
+                </a>
 
-            <a className="mr-6" href="/CashFlow">
-                Entradas e Saídas
-            </a>
+                <a className="mr-6" href="/CashFlow">
+                    Entradas e Saídas
+                </a>
 
-            <a className="mr-6" href={'/ServiceOrder'}>
-                Ordem de Serviço
-            </a>
+                <a className="mr-6" href={'/ServiceOrder'}>
+                    Ordem de Serviço
+                </a>
+            </div>
         </div>
     )
 }
