9aa3cf7f5f4537fbd5896aa4b72875ac07ac0ffc
kevin-krause <contatokevinkrause@gmail.com>
2023-05-21 03:02:55 -0300

    header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
         <div className="max-w-full p-6 flex items-center ml-6 mr-6 mt-3 bg-gradient-to-tl from-gray-900 via-gray-900 to-black rounded-lg text-zinc-100 justify-between">
             <a className="mr-6" href={'/'}>
                 <img
-                    className="w-[100px] h-[16x]"
+                    className="w-[100px] h-[18,75x]"
                     src=".\favicon.ico"
                     alt="logo"
                 />
